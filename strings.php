5becd51561c53022d8dff4b2ca8bb8a46ace1209
SimonSmale <simon@smalemail.net>
2016-04-22 16:28:47 +0100

    Add Script 6 - Strings

---
+++ strings.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Strings</title>
+</head>
+<body>
+<?php
+
+# Script 1.6 - strings.php
+# Created 22 April 2016
+# Created by Simon Smale
+# This script shows the use of String Variables.
+
+$firstName = 'Simon';
+    $secondName = 'Smale';
+    $book = 'Strings';
+    
+    echo"<p>The web page <em>$book</em>
+    was writen by $firstName $secondName.</p>"
+
+?>
+</body>
+</html>
