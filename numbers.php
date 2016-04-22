71ac64920ec3c43313ec08889fe2a4fa2bcee7a4
SimonSmale <simon@smalemail.net>
2016-04-22 16:50:03 +0100

    Add Script 8 - Numbers

---
+++ numbers.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Numbers</title>
+</head>
+<body>
+<?php
+
+# Script 1.8 - numbers.php
+# Created 22 April 2016
+# Created by Simon Smale
+# This script shows the use of numbers and arithmatic.
+
+//set the variables
+    $qty = 30; // number they buy
+    $price = 119.95;
+    $taxRate = .2; //20% tax (UK VAT)
+    
+    //calc the total
+    $total = $qty*$price;
+    $total = $total + ($total * $taxRate);
+    // calulate then add tax
+    
+    //Format
+    $total = number_format ($total, 2);
+    
+    //print to screen
+    echo '<p>You are purchasing <b>' .$qty . '</b> widget(s) at a cost of <b>£'. $price . '</b> each. \nWith VAT the total comes to <b>£'. $total . '</b>.</p>';
+</body>
+</html>
