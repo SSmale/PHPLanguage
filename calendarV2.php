49bda12643bd5583ea5ca92ca3c74643ad46405c
SimonSmale <simon@smalemail.net>
2016-05-19 15:50:39 +0100

    Add Calendar v2.php

---
+++ calendarV2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Calendar v2</title>
+</head>
+<body>
+    <form action="calendar.php" method="post">
+<?php
+
+# Script 2.6 - calendar.php
+# Created 19 May 2016
+# Created by Simon Smale
+# This script generates a calender selector using php.
+#v2 using for loops in place for the foreach.
+
+        //Make the month array
+$months = array (1 => 'Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Nov', 'Dec');
+                
+        //Generate Form
+        //days
+        echo '<select name="day">';
+        for($day = 1; $day <=31; $day++){
+            echo "<option value=\"$day\"> $day </option>\n";
+        }
+        echo '</select>';
+        
+                //months
+        echo '<select name="month">';
+        foreach($months as $value){
+            echo "<option value=\"$value\"> $value </option>\n";
+        }
+        echo '</select>';
+        
+                //years
+        echo '<select name="year">';
+        for($year =1900; $year <=2100; $year++){
+            echo "<option value=\"$year\"> $year </option>\n";
+        }
+        echo '</select>';
+?>
+    </form>
+    
+</body>
+</html>
