0708979c9ad575b99dca1dbda75def4486d83d92
SimonSmale <simon@smalemail.net>
2016-05-19 16:29:47 +0100

    Write index.php

---
+++ Part 3/index.php
@@ -0,0 +1,13 @@
+<?php 
+#index.php
+$pageTitle = "Welcome to Simon's PHP Sandbox";
+include ('Templates/header.html');
+?>
+
+<h1>Welcome to my Home Page</h1>
+
+<p>FILL ME UP PLEASE!!!!</p>
+
+<?php
+incluse ('Templates/fotter.html');
+?>
